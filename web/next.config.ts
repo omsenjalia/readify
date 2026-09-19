f5a3c8d513feab7c9897a8e88d9682eabf3b3ac5
om <omsenjalia@gmail.com>
2026-09-19 16:55:17 +0530

    Update next.config.ts

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,7 +1,7 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  allowedDevOrigins: ['dono-03.danbot.host'],
+}
 
-export default nextConfig;
+export default nextConfig
